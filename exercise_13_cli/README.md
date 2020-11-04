949d6b68eb6964d5ae4b069a61f8b719853f3646
Halil ibrahim Koc <halil66272@gmail.com>
2020-11-04 17:29:08 +0300

    communication between components
    We used eventBus and $emit and props property for provide communicate between components. And used bootstrap for buttons.

---
+++ exercise_13_cli/README.md
@@ -0,0 +1,18 @@
+# communication-between-components
+
+> A Vue.js project that we used eventBus and $emit and props property for provide communicate between components. And used bootstrap for buttons.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ exercise_13_cli/src/components/UserEdit.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-md-s6">
+    <h3>Child Component 2</h3>
+    <p>I am child component 2 which inside of Parent Component.</p>
+    <p> {{title}} :: {{switchName()}} </p>
+    <p> IQ level : {{iqlevel}} </p>
+     <button type="button" class="btn btn-primary" @click="sendToParent">Send To Parent</button>
+     <button type="button" class="btn btn-success" @click="increaseIq">Increase IQ</button>
+     
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main"
+
+export default {
+  data(){
+    return{
+      iqlevel : 3
+    }
+  },
+  props : {
+    title : {
+      type : String,
+      default : "This is default title"
+    },
+  },
+  methods : {
+    switchName(){
+      return this.title.split("").reverse().join("");
+    },
+    sendToParent(){
+      this.$emit("dataChild2", "Child 2" );
+    },
+    increaseIq(){
+      this.iqlevel = 30;
+      eventBus.$emit("iqlevel", this.iqlevel);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    background-color: lightgoldenrodyellow;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
+
